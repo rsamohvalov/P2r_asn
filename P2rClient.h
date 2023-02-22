2a3a67a1e44e36de2f4991ab2ec09c007338d73f
R.Samokhvalov <r.samokhvalov@kryptonite.ru>
2023-02-22 13:46:59 +0300

    del

--- P2rClient.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <pthread.h>
-
-void *P2r_client_test(void *);
